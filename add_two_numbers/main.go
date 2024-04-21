bfde1719c91ecf24acedc473b8226dbdff1e39d5
wsy0414 <w226s1460y@gmail.com>
2024-04-21 19:55:27 +0800

    refactor ListNode constructor method

--- add_two_numbers/main.go
+++ add_two_numbers/main.go
@@ -3,80 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	l1 := ListNode{
-		Val: 2,
-		Next: &ListNode{
-			Val: 4,
-			Next: &ListNode{
-				Val: 3,
-			},
-		},
-	}
-
-	l2 := ListNode{
-		Val: 5,
-		Next: &ListNode{
-			Val: 6,
-			Next: &ListNode{
-				Val: 4,
-			},
-		},
-	}
-
-	addTwoNumbers(&l1, &l2).toString()
-
-	l1 = ListNode{
-		Val: 0,
-	}
-
-	l2 = ListNode{
-		Val: 0,
-	}
-
-	addTwoNumbers(&l1, &l2).toString()
-
-	l1 = ListNode{
-		Val: 9,
-		Next: &ListNode{
-			Val: 9,
-			Next: &ListNode{
-				Val: 9,
-				Next: &ListNode{
-					Val: 9,
-					Next: &ListNode{
-						Val: 9,
-						Next: &ListNode{
-							Val: 9,
-							Next: &ListNode{
-								Val: 9,
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	l2 = ListNode{
-		Val: 9,
-		Next: &ListNode{
-			Val: 9,
-			Next: &ListNode{
-				Val: 9,
-				Next: &ListNode{
-					Val: 9,
-				},
-			},
-		},
-	}
-
-	addTwoNumbers(&l1, &l2).toString()
+	addTwoNumbers(NewListNode([]int{2, 4, 3}), NewListNode([]int{5, 6, 4})).toString()
+	addTwoNumbers(NewListNode([]int{0}), NewListNode([]int{0})).toString()
+	addTwoNumbers(NewListNode([]int{9, 9, 9, 9, 9, 9, 9}), NewListNode([]int{9, 9, 9, 9})).toString()
 }
 
 // Definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func NewListNode(list []int) *ListNode {
+	return newListNode(list, 0)
+}
+
+func newListNode(list []int, i int) *ListNode {
+	if i == len(list)-1 {
+		return &ListNode{
+			Val: list[len(list)-1],
+		}
+	}
+
+	return &ListNode{
+		Val:  list[i],
+		Next: newListNode(list, i+1),
+	}
 }
 
 func (listNode *ListNode) toString() string {
